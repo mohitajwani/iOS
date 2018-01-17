2f6a1d3e2626150bfa966eec5054b038c6d6ec4a
Mohit Ajwani <mohitajwani@gmail.com>
2018-01-17 23:24:26 +0530

    Mini Excercise

--- SwiftTutorials/MyPlayground.playground/Contents.swift
+++ SwiftTutorials/MyPlayground.playground/Contents.swift
@@ -74,3 +74,27 @@ var counter: Int = 0
 counter += 1
 counter *= 3
 
+
+/*
+ Mini-exercises
+ If you haven’t been following along with the code in Xcode, now’s the time to create a new playground and try some exercises to test yourself!
+ 
+ Declare a constant of type Int called myAge and set it to your age.
+ Declare a variable of type Double called averageAge. Initially, set it to your own age. Then, set it to the average of your age and my own age of 30.
+ Create a constant called testNumber and initialize it with whatever integer you’d like. Next, create another constant called evenOdd and set it equal to testNumber modulo 2. Now change testNumber to various numbers. What do you notice about evenOdd?
+ Create a variable called answer and initialize it with the value 0. Increment it by 1. Add 10 to it. Multiply it by 10. Then, shift it to the right by 3. After all of these operations, what’s the answer?
+*/
+
+let myAge: Int = 27
+
+var averageAge: Double = 27.0
+averageAge = (averageAge+30)/2
+
+let testNumber: Int = 33
+let evenOdd: Int = testNumber%2
+
+var answer: Int = 0
+answer += 1
+answer += 10
+answer >> 3
+
