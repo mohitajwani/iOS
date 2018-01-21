e714565e87772b9017f9b2c80c8b46a28a008cb1
Mohit Ajwani <mohitajwani@gmail.com>
2018-01-21 18:39:53 +0530

    Sample iOS App

---
+++ SampleApp/SampleApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Admin on 21/01/18.
+//  Copyright © 2018 Mohit Ajwani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
