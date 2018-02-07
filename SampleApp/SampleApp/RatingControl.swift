81b13af182a22f97a86bb4d5633369a71f7b1f71
Mohit Ajwani <mohitajwani@gmail.com>
2018-02-07 22:55:13 +0530

    Added RatingControl file

---
+++ SampleApp/SampleApp/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  SampleApp
+//
+//  Created by Admin on 07/02/18.
+//  Copyright © 2018 Mohit Ajwani. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
