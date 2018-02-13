09334935c2ee1409b44f81f95ff8198fe5dff163
Mohit Ajwani <mohitajwani@gmail.com>
2018-02-13 22:07:03 +0530

    setup rating bar

--- SampleApp/SampleApp/RatingControl.swift
+++ SampleApp/SampleApp/RatingControl.swift
@@ -14,10 +14,12 @@ class RatingControl: UIStackView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupButtons()
     }
     
     required init(coder: NSCoder) {
         super.init(coder: coder)
+        setupButtons()
         
     }
     
@@ -28,9 +30,13 @@ class RatingControl: UIStackView {
         // Create the button
         let button = UIButton()
         button.backgroundColor = UIColor.red
+        
         // Add constraints
         button.translatesAutoresizingMaskIntoConstraints = false
         button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
         button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        
+        // Add the button to the stack
+        addArrangedSubview(button)
     }
 }
